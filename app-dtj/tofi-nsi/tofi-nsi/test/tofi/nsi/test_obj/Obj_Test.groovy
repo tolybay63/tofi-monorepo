67e4673730d873394d2d4eb9859ec05905357e51
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-21 17:13:20 +0500

    upd && add && del

--- app-dtj/tofi-nsi/tofi-nsi/test/tofi/nsi/test_obj/Obj_Test.groovy
+++ app-dtj/tofi-nsi/tofi-nsi/test/tofi/nsi/test_obj/Obj_Test.groovy
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 class Obj_Test extends Apx_Test {
 
     @Test
-    public void jsonrpc1() throws Exception {
+    void jsonrpc1() throws Exception {
         Map<String, Store>  map = apx.execJsonRpc("api", "data/loadObj", [1000]) as Map<String, Store>
         mdb.outMap(map)
         map.result.records.forEach {
