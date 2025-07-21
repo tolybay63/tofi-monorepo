a83a6de5761e071419246a7241e203c57e33d78a
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-21 22:13:13 +0500

    Project [DTJ Service -Objects]

---
+++ app-dtj/dtj-object/dtj-object/src/dtj/object/action/DeletefileAction.java
@@ -0,0 +1,23 @@
+package dtj.object.action;
+
+import jandcode.commons.UtFile;
+import jandcode.core.web.action.BaseAction;
+
+import java.io.File;
+
+public class DeletefileAction extends BaseAction {
+
+
+    protected void onExec() throws Exception {
+        String tempDir = System.getProperty("java.io.tmpdir");
+
+        String fn = getReq().getParams().getString("filename");
+        fn = fn.replace(UtFile.ext(fn), "pdf");
+        String destPath = tempDir + fn;
+        File file = new File(destPath);
+        if (file.delete())
+            getReq().render("Ok");
+        else
+            getReq().render("Error");
+    }
+}
