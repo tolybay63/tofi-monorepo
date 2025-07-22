927cd19e4476154ce66ee5bfdd4edae703bad892
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-22 05:54:59 +0500

    Project [DTJ App]

---
+++ app-dtj/dtj-app/frontend/src/boot/axios.js
@@ -0,0 +1,48 @@
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
+//const api = axios.create({ baseURL: 'https://api.example.com' })
+
+const appAdmURL = "http://192.168.1.20:9172"
+const appModelURL = "http://192.168.1.20:9173"
+const appDataUserURL = "http://192.168.1.20:9174"
+const appDataURL = "http://192.168.1.20:9175"
+const appNSIURL = "http://192.168.1.20:9176"
+
+
+//const appAdmURL = "http://localhost:9172"
+//const appModelURL = "http://localhost:9173"
+//const appDataUserURL = "http://localhost:9174"
+//const appDataURL = "http://localhost:9175"
+//const appNSIURL = "http://localhost:9176"
+
+
+let url = "http://localhost:8080"
+if (process.env.NODE_ENV === 'production') {
+  url = "http://192.168.1.20:9171"
+//  url = "http://localhost:9171"
+}
+
+const baseURL = url + "/api"
+const api = axios.create({ baseURL: baseURL })
+
+
+export default boot(({ app }) => {
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+
+  app.config.globalProperties.$api = api
+  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
+  //       so you can easily perform requests against your app's API
+})
+
+export { api, baseURL, appAdmURL, appModelURL, appDataUserURL, appDataURL, appNSIURL }
---
+++ app-dtj/dtj-app/frontend/src/pages/IndexPage.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="body">
+    <q-page class="flex flex-center">
+
+      <div style="height: 50%; width: 80%">
+
+        <div class="row q-ma-md justify-center">
+            <!-- adm -->
+            <q-btn
+              rounded
+              class="bg-orange text-white text-bold q-ma-md"
+              style="height: 200px; width: 350px; font-size: 26px; opacity: 0.7"
+              @click="fnAdm()"
+            >
+              <div class="column">
+                <div>
+                  <q-avatar icon="manage_accounts" color="green"></q-avatar>
+                  {{ $t("appAdmName") }}
+                </div>
+              </div>
+            </q-btn>
+        </div>
+
+
+        <div class="row q-ma-md justify-center">
+            <!-- model -->
+            <q-btn
+              rounded
+              class="bg-blue text-white text-bold q-ma-md"
+              style="height: 200px; width: 350px; font-size: 26px; opacity: 0.7"
+              @click="fnMdl()"
+            >
+              <div class="column">
+                <div>
+                  <q-avatar icon="recycling" color="orange"></q-avatar>
+                  {{ $t("appModelName") }}
+                </div>
+              </div>
+            </q-btn>
+
+            <!-- data -->
+            <q-btn
+              rounded
+              class="bg-blue text-white text-bold q-ma-md"
+              style="height: 200px; width: 350px; font-size: 26px; opacity: 0.7"
+              @click="fnData()"
+            >
+              <div class="column">
+                <div>
+                  <q-avatar icon="edit_note" color="orange"></q-avatar>
+                  {{ $t("appDataName") }}
+                </div>
+              </div>
+            </q-btn>
+        </div>
+
+        <div class="row q-ma-md justify-center">
+            <!-- User Data -->
+            <q-btn
+              rounded
+              class="bg-green text-white text-bold q-ma-md"
+              style="height: 200px; width: 350px; font-size: 24px; opacity: 0.7"
+              @click="fnUserData()"
+            >
+              <div class="column">
+                <div>
+                  <q-avatar icon="contacts" color="orange"></q-avatar>
+                  {{ $t("appUserDataName") }}
+                </div>
+              </div>
+            </q-btn>
+
+            <!-- NSI -->
+            <q-btn
+              rounded
+              class="bg-green text-white text-bold q-ma-md"
+              style="height: 200px; width: 350px; font-size: 26px; opacity: 0.7"
+              @click="fnNSI()"
+            >
+              <div class="column">
+                <div>
+                  <q-avatar icon="menu_book" color="orange"></q-avatar>
+                  {{ $t("appNSIName") }}
+                </div>
+              </div>
+            </q-btn>
+
+        </div>
+
+      </div>
+
+    </q-page>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import {
+  appAdmURL,
+  appModelURL,
+  appDataUserURL,
+  appDataURL, appNSIURL
+} from "boot/axios";
+
+export default defineComponent({
+  name: "IndexPage",
+
+  methods: {
+    fnAdm() {
+      open(appAdmURL, "_self");
+    },
+
+    fnMdl() {
+      open(appModelURL, "_self");
+    },
+
+    fnUserData() {
+      open(appDataUserURL, "_self");
+    },
+
+    fnData() {
+      open(appDataURL, "_self");
+    },
+
+    fnNSI() {
+      open(appNSIURL, "_self");
+    },
+
+
+  },
+});
+</script>
+
+<style scoped>
+.body {
+  background-image: url("../assets/ms.png");
+}
+
+button:hover {
+  transform: skew(-10deg);
+  box-shadow: 0 5px 25px 0 rgba(0, 0, 0, 0.7);
+}
+</style>
