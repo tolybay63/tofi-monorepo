93f39f9c11901ff7b465ff81a73f038dbca51c8f
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-22 05:54:59 +0500

    Project [DTJ App]

---
+++ app-dtj/dtj-app/frontend/src/i18n/kk/index.js
@@ -0,0 +1,32 @@
+// This is just an example,
+// so you can safely delete all default props below
+// noinspection AllyPlainJsInspection
+
+export default {
+  lang: "kz",
+  appName: "ТНФӨ платформасы",
+  //
+  appModelName: "Модельдеу ортасы",
+  appAdmName: "Әкімшілік ортасы",
+  appUserDataName: "Пайдаланушының жеке кабинеті",
+  appDataName: "Деректермен жұмыс істеу қызметі",
+  appNSIName: "Нормативтік анықтамалық ақпарат",
+  appMonitoringName: "Балық аулау резервуарларының мониторингі",
+
+
+  appMinIO: "Файлды сақтау",
+  appCubesName: "Текшелермен жұмыс істеу қызметі",
+  appAlgSrvName: "Алгоритмдік сервер",
+  appSrvAlgExec: "Алгоритмді орындау қызметі",
+  appPulsarName: "Хабар алмасу қызметі",
+  appSrvETLName: "Деректерді жүктеу қызметі (ETL)",
+  appConfigName: "Архитектураны конфигурациялау қызметі",
+  appBIName: "Аналитикалық сервер (BI)",
+  appEventsName: "Іс-шаралар",
+
+  //
+  company: 'ЖШС "Фактор" жүйелі зерттеулер компаниясы"',
+  chooseLanguage: "Тіл таңдау",
+  nothing_here: "Ойбай. Мұнда ештеңе жоқ...",
+  home: "Үйге",
+};
