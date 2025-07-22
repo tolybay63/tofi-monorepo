c3ac6fd5a48ff2bc886e820540deb3c436f47760
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-22 22:00:57 +0500

    upd && add

--- app-dtj/dtj-object/dtj-object/src/dtj/object/dao/DataDao.groovy
+++ app-dtj/dtj-object/dtj-object/src/dtj/object/dao/DataDao.groovy
@@ -6,6 +6,10 @@ import jandcode.core.dao.DaoMethod
 import jandcode.core.dbm.mdb.BaseMdbUtils
 import jandcode.core.store.Store
 import tofi.api.dta.ApiNSIData
+import tofi.api.dta.ApiObjectData
+import tofi.api.dta.ApiOrgStructureData
+import tofi.api.dta.ApiPersonnalData
+import tofi.api.dta.ApiPlanData
 import tofi.api.dta.ApiUserData
 import tofi.api.mdl.ApiMeta
 import tofi.apinator.ApinatorApi
@@ -14,7 +18,6 @@ import tofi.apinator.ApinatorService
 
 @CompileStatic
 class DataDao extends BaseMdbUtils {
-
 
     ApinatorApi apiMeta() {
         return app.bean(ApinatorService).getApi("meta")
@@ -25,31 +28,87 @@ class DataDao extends BaseMdbUtils {
     ApinatorApi apiNSIData() {
         return app.bean(ApinatorService).getApi("nsidata")
     }
+    ApinatorApi apiObjectData() {
+        return app.bean(ApinatorService).getApi("objectdata")
+    }
+    ApinatorApi apiPlanData() {
+        return app.bean(ApinatorService).getApi("plandata")
+    }
+    ApinatorApi apiPersonnalData() {
+        return app.bean(ApinatorService).getApi("personnaldata")
+    }
+    ApinatorApi apiOrgStructureData() {
+        return app.bean(ApinatorService).getApi("orgstructuredata")
+    }
 
-
-
-
+    /**
+     *
+     * @param codFactor: код фактора
+     * @return  Набор значений указанного фактора
+     */
     @DaoMethod
-    Store loadTyp() {
-        String sql = """
-            select * from Typ t, TypVer v
-            where t.id=v.ownerVer and v.lastVer=1
-        """
-        return loadSqlMeta(sql, "Typ.object")
+    Store loadFactorValForSelect(String codPropOrFactor) {
+        Map<String, Long> map
+        String sql
+        if (codPropOrFactor.startsWith("Prop_")) {
+            map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Prop", codPropOrFactor, "")
+            if (map.isEmpty())
+                throw new XError("NotFoundCod@${codPropOrFactor}")
+            sql = """
+                select f.id, f.name, p.id as pv
+                from propval p, factor f
+                where p.factorval=f.id and p.prop=${map.get(codPropOrFactor)}
+            """
+        } else if (codPropOrFactor.startsWith("Factor_")) {
+            map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Factor", codPropOrFactor, "")
+            if (map.isEmpty())
+                throw new XError("NotFoundCod@${codPropOrFactor}")
+            sql = """
+                select f.id, f.name, p.id as pv, f.parent
+                from propval p, factor f 
+                where p.factorval=f.id and f.parent=${map.get(codPropOrFactor)}
+            """
+        } else {
+            throw new XError("Неисвезстная сущность")
+        }
+        return loadSqlMeta(sql, "")
     }
 
 
-        @DaoMethod
-    Store loadObj(String codCls, String model) {
-        Map<String, Long> map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Cls", codCls, "")
+    /**
+     *
+     * @param codClsOr: Код класса или код типа
+     * @param model:    Идентификатор сервиса (nsidata,plandata,personnaldata,...)
+     * @return  Набор записей
+     */
+    @DaoMethod
+    Store loadObjList(String codClsOrTyp, String model) {
+        Map<String, Long> map
+        String sql
+        if (codClsOrTyp.startsWith("Cls_")) {
+            map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Cls", codClsOrTyp, "")
+            if (map.isEmpty())
+                throw new XError("NotFoundCod@${codClsOrTyp}")
+            sql = """
+                select o.id, o.cls, v.name 
+                from Obj o, ObjVer v
+                where o.id=v.ownerVer and v.lastVer=1 and o.cls=${map.get(codClsOrTyp)}
+            """
+        } else if (codClsOrTyp.startsWith("Typ_")) {
+            map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Typ", codClsOrTyp, "")
+            if (map.isEmpty())
+                throw new XError("NotFoundCod@${codClsOrTyp}")
+            sql = """
+                select o.id, o.cls, v.name 
+                from Obj o, ObjVer v
+                where o.id=v.ownerVer and v.lastVer=1 and o.cls in (
+                  select id from Cls where typ=${map.get(codClsOrTyp)}
+                )
+            """
+        } else
+            throw new XError("Неисвезстная сущность")
 
-        String sql = """
-            select * from Obj o, ObjVer v
-            where o.id=v.ownerVer and v.lastVer=1 and o.cls=${map.get(codCls)}
-        """
-
-        Store st = loadSqlService(sql, "Obj.object", model)
-
+        Store st = loadSqlService(sql, "", model)
         return st
     }
 
@@ -67,8 +126,13 @@ class DataDao extends BaseMdbUtils {
         else if (model.equalsIgnoreCase("nsidata"))
             return apiNSIData().get(ApiNSIData).loadSql(sql, domain)
         else if (model.equalsIgnoreCase("objectdata"))
-            return apiNSIData().get(ApiNSIData).loadSql(sql, domain)
-
+            return apiObjectData().get(ApiObjectData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("plandata"))
+            return apiPlanData().get(ApiPlanData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("personnaldata"))
+            return apiPersonnalData().get(ApiPersonnalData).loadSql(sql, domain)
+        else if (model.equalsIgnoreCase("orgstructuredata"))
+            return apiOrgStructureData().get(ApiOrgStructureData).loadSql(sql, domain)
         else
             throw new XError("Unknown model [${model}]")
     }
