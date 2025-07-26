37858e2431bd533b8919c22b78dc4f613b0fd7c6
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-26 19:58:11 +0500

    rename folder

---
+++ app-dtj/dtj-orgstructure/dtj-orgstructure/src/dtj/orgstructure/action/DownFileRender.java
@@ -0,0 +1,28 @@
+package dtj.orgstructure.action;
+
+import jandcode.commons.UtFile;
+import jandcode.core.web.UtWeb;
+import jandcode.core.web.render.BaseRender;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DownFileRender extends BaseRender {
+
+    @Override
+    protected void onRender(Object data) throws Exception {
+        DownFile md = (DownFile) data;
+
+        UtWeb.setHeaderDownload(getRequest(), md.getClientName(), md.getFile().length());
+
+        InputStream stmSource = new FileInputStream(md.getFile());
+        OutputStream stmDest = getRequest().getOutStream();
+        try {
+            UtFile.copyStream(stmSource, stmDest);
+        } finally {
+            stmSource.close();
+        }
+    }
+
+}
