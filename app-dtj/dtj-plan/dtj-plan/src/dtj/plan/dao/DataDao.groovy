db6e12f82423aaaab3dd3d19cfd48167aeda9cf5
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-07-30 19:46:11 +0500

    upd

--- app-dtj/dtj-plan/dtj-plan/src/dtj/plan/dao/DataDao.groovy
+++ app-dtj/dtj-plan/dtj-plan/src/dtj/plan/dao/DataDao.groovy
@@ -1,10 +1,12 @@
 package dtj.plan.dao
 
 import groovy.transform.CompileStatic
+import jandcode.commons.UtCnv
 import jandcode.commons.error.XError
 import jandcode.core.dao.DaoMethod
 import jandcode.core.dbm.mdb.BaseMdbUtils
 import jandcode.core.store.Store
+import jandcode.core.store.StoreIndex
 import tofi.api.dta.ApiNSIData
 import tofi.api.dta.ApiPersonnalData
 import tofi.api.dta.ApiUserData
@@ -48,6 +50,101 @@ class DataDao extends BaseMdbUtils {
 
     }
 
+    private Set<Object> getIdsObjLocation(long obj) {
+        Store st = loadSqlService("""
+           WITH RECURSIVE r AS (
+               SELECT o.id, v.objParent as parent
+               FROM Obj o, ObjVer v
+               WHERE o.id=v.ownerver and v.lastver=1 and v.objParent=${obj}
+               UNION ALL
+               SELECT t.*
+               FROM ( SELECT o.id, v.objParent as parent
+                      FROM Obj o, ObjVer v
+                      WHERE o.id=v.ownerver and v.lastver=1
+                    ) t
+                  JOIN r
+                      ON t.parent = r.id
+           ),
+           o as (
+           SELECT o.id, v.objParent as parent
+           FROM Obj o, ObjVer v
+           WHERE o.id=v.ownerver and v.lastver=1 and o.id=${obj}
+           )
+           SELECT * FROM o
+           UNION ALL
+           SELECT * FROM r
+           where 0=0
+        """, "", "orgstructuredata")
+
+        return st.getUniqueValues("id")
+    }
+
+    @DaoMethod
+    Store loadPlan(Map<String, Object> params) {
+        Map<String, Long> map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Typ", "Typ_WorkPlan", "")
+        Store st = mdb.createStore("Obj.Plan")
+
+        Store stCls = loadSqlMeta("""
+            select c.id , v.name
+            from Cls c, ClsVer v
+            where c.id=v.ownerVer and v.lastVer=1 and typ=${map.get("Typ_WorkPlan")}
+        """, "")
+        Set<Object> idsCls = stCls.getUniqueValues("id")
+        StoreIndex indCls = stCls.getIndex("id")    //????
+        String whe = ""
+        if (params.containsKey("id"))
+                whe = "o.id=${UtCnv.toLong(params.get("id"))}"
+        else {
+            whe = "o.cls in (${idsCls.join(",")})"
+            Set<Object> idsObjLocation = getIdsObjLocation(UtCnv.toLong(params.get("objLocation")))
+            //todo whe += ...
+        }
+
+        map = apiMeta().get(ApiMeta).getIdFromCodOfEntity("Prop", "", "Prop_")
+
+        mdb.loadQuery(st, """
+            select o.id, o.cls, v.name, null as nameCls,
+                v1.id as idLocationClsSection,  --??
+                v2.id as idObject,  --??
+                v3.id as idStartKm, v3.numberVal as StartKm,
+                v4.id as idFinishKm, v4.numberVal as FinishKm,
+                v5.id as idStartPicket, v5.numberVal as StartPicket,
+                v6.id as idFinishPicket, v6.numberVal as FinishPicket,
+                v7.id as idPlanDateEnd, v7.dateTimeVal as PlanDateEnd,
+                v8.id as idUser, v8.propVal as pvUser, v8.obj as objUser,   --???
+                v9.id as idCreatedAt, v9.dateTimeVal as CreatedAt,
+                v10.id as idUpdatedAt, v10.dateTimeVal as UpdatedAt,
+                v11.id as idLocationClsSection --??
+            from Obj o 
+                left join ObjVer v on o.id=v.ownerver and v.lastver=1
+
+                left join DataProp d1 on d1.objorrelobj=o.id and d1.prop=:Prop_LocationClsSection
+                left join DataPropVal v1 on d1.id=v1.dataprop
+                left join DataProp d2 on d2.objorrelobj=o.id and d2.prop=:Prop_Object
+                left join DataPropVal v2 on d2.id=v2.dataprop
+                left join DataProp d3 on d3.objorrelobj=o.id and d3.prop=:Prop_StartKm
+                left join DataPropVal v3 on d3.id=v3.dataprop
+                left join DataProp d4 on d4.objorrelobj=o.id and d4.prop=:Prop_FinishKm
+                left join DataPropVal v4 on d4.id=v4.dataprop
+                left join DataProp d5 on d5.objorrelobj=o.id and d5.prop=:Prop_StartPicket
+                left join DataPropVal v5 on d5.id=v5.dataprop
+                left join DataProp d6 on d6.objorrelobj=o.id and d6.prop=:Prop_FinishPicket
+                left join DataPropVal v6 on d6.id=v6.dataprop
+                left join DataProp d7 on d7.objorrelobj=o.id and d7.prop=:Prop_PlanDateEnd
+                left join DataPropVal v7 on d7.id=v7.dataprop
+                left join DataProp d8 on d8.objorrelobj=o.id and d8.prop=:Prop_User
+                left join DataPropVal v8 on d8.id=v8.dataprop
+                left join DataProp d9 on d9.objorrelobj=o.id and d9.prop=:Prop_CreatedAt
+                left join DataPropVal v9 on d9.id=v9.dataprop
+                left join DataProp d10 on d10.objorrelobj=o.id and d10.prop=:Prop_UpdatedAt
+                left join DataPropVal v10 on d10.id=v10.dataprop
+                left join DataProp d11 on d11.objorrelobj=o.id and d11.prop=:Prop_LocationClsSection
+                left join DataPropVal v11 on d11.id=v11.dataprop
+            where ${whe}
+        """, map)
+
+        return null
+    }
 
 
 
