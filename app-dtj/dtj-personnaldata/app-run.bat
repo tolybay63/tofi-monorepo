dd24086e5fdabcf55e5a7bc0a9e443cd15e12a70
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-08-12 06:26:08 +0500

    add

---
+++ app-dtj/dtj-personnaldata/app-run.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem in JC_JVM java parameters -Dxxx=yyy
+
+set P1=%~dp0_jc/app-run-classpath.txt
+if not exist %P1% (
+    call jc prepare
+)
+set CP=@%P1%
+
+set JVM=
+set JVM=%JVM% -cp %CP%
+set JVM=%JVM% -Djandcode.app.appdir=%~dp0
+set JVM=%JVM% -Djandcode.app.cmdname=%~n0
+set JVM=%JVM% -Dfile.encoding=UTF-8
+set MAIN=dtj.personnaldata.main.Main
+
+call jc @ java %JVM% %JC_JVM% %MAIN% %*
