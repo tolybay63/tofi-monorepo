5242d23eb6099b852c814761998b027c35444e55
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-12-12 19:09:22 +0500

    Delete order

--- order/src/main/java/kz/ksifactor/client/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.ksifactor.client.repository;
-
-import kz.ksifactor.client.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
-}
