d34d831542b81c106c88f19dc160199aace7cbd1
tolybay63 <tolybay.zh@gmail.com>
2026-07-03 12:37:29 +0500

    upd

--- tofi-adm/tofi-adm/test/tofi/adm/Permis_Test.groovy
+++ tofi-adm/tofi-adm/test/tofi/adm/Permis_Test.groovy
@@ -17,7 +17,15 @@ class Permis_Test extends Apx_Test {
         String psw = "Fish@2026"
         //String psw = "Qwerty123!"
         String pswHash = argon2.hash(3, 65536, 4, psw.toCharArray());
+        boolean pswValid = argon2.verify(pswHash, psw.toCharArray());
         println(pswHash)
+        println(pswValid)
+        //
+        String tst = '$argon2id$v=19$m=65536,t=3,p=4$IRaAm4sXLb4MNFuDR9ouzw$o8ohVPblvfwvBr3Fz79IrF9d2wuf/qqLiqp19vTRa1Y'
+
+        pswValid = argon2.verify(tst, psw.toCharArray());
+        println(pswValid)
+
 //$argon2id$v=19$m=65536,t=3,p=4$IRaAm4sXLb4MNFuDR9ouzw$o8ohVPblvfwvBr3Fz79IrF9d2wuf/qqLiqp19vTRa1Y
 //$argon2id$v=19$m=65536,t=3,p=4$II2ZubTT5wObDQmI5LCD0w$YnJTvpAx3iRbVNP0ogCMv430VPMa8HYRVAbykoS1STI
 
