9e743fe0e0111afa4eda7736981010b33531b940
Tolybay.Kuanov <tolybay.zh@gmail.com>
2026-07-26 06:20:32 +0500

    add && upd && del

--- app-fish/fish-personnel/frontend/src/boot/i18n.js
+++ app-fish/fish-personnel/frontend/src/boot/i18n.js
@@ -1,6 +1,6 @@
 import {defineBoot} from '#q-app/wrappers'
 import {createI18n} from 'vue-i18n'
-import messages from "@intlify/unplugin-vue-i18n/messages";
+import messages from 'src/i18n'
 
 
 let lang = localStorage.getItem('curLang')
