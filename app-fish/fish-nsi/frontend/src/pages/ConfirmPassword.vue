23ab55986b5a0c04dcd8382582347982fae3d135
Tolybay.Kuanov <tolybay.zh@gmail.com>
2026-08-18 14:06:21 +0500

    upd

--- app-fish/fish-nsi/frontend/src/pages/ConfirmPassword.vue
+++ app-fish/fish-nsi/frontend/src/pages/ConfirmPassword.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="fixed-center text-center q-pa-md">
-    <!-- Иконка и статус успешной активации -->
     <div v-if="success">
       <q-icon name="check_circle" color="positive" size="80px" />
       <div class="text-h5 q-mt-md text-weight-bold text-grey-9">Пароль успешно активирован!</div>
@@ -12,11 +11,10 @@
         label="На главную страницу"
         class="q-mt-xl q-px-xl text-weight-bold"
         size="lg"
-        @click="mainApp()"
+        @click="mainApp"
       />
     </div>
 
-    <!-- Ошибка (если зашли повторно или ссылка битая) -->
     <div v-else-if="error">
       <q-icon name="warning" color="warning" size="80px" />
       <div class="text-h5 q-mt-md text-weight-bold text-grey-9">Внимание</div>
@@ -27,11 +25,10 @@
         color="grey-7"
         label="На главную страницу"
         class="q-mt-xl q-px-xl"
-        @click="mainApp()"
+        @click="mainApp"
       />
     </div>
 
-    <!-- Спиннер во время запроса -->
     <div v-else>
       <q-spinner-cube color="primary" size="60px" />
       <div class="text-h6 text-grey-7 q-mt-md">Проверка ссылки подтверждения...</div>
@@ -39,50 +36,41 @@
   </div>
 </template>
 
-<script>
-import axios from 'axios'
-import {api, urlMainApp} from '../boot/axios'
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { api, urlMainApp } from '@/boot/axios'
 
+const route = useRoute()
 
-export default {
-  name: 'ConfirmPassword',
-  data () {
-    return {
-      success: false,
-      error: false,
-      errorMessage: ''
-    }
-  },
+const success = ref(false)
+const error = ref(false)
+const errorMessage = ref('')
 
-  methods: {
-
-    mainApp() {
-      window.open(urlMainApp, '_self')
-    }
-
-  },
-
-
-  mounted () {
-    const loginParam = this.$route.query.login
-
-    if (!loginParam) {
-      this.error = true
-      this.errorMessage = 'Некорректная ссылка: отсутствует логин.'
-      return
-    }
-
-    axios.post(api.defaults.baseURL, {
-      method: 'psw/confirmPasswd',
-      params: [{ login: loginParam }]
-    })
-      .then(() => {
-        this.success = true
-      })
-      .catch((err) => {
-        this.error = true
-        this.errorMessage = err.response?.data?.error?.message || 'Запрос на сброс пароля не создавался или уже активирован.'
-      })
-  }
+const mainApp = () => {
+  window.open(urlMainApp, '_self')
 }
+
+onMounted(() => {
+  const loginParam = route.query.login
+
+  if (!loginParam) {
+    error.value = true
+    errorMessage.value = 'Некорректная ссылка: отсутствует логин.'
+    return
+  }
+
+  // Используем штатный экземпляр api (точно так же, как во всем проекте)
+  api.post('', {
+    method: 'psw/confirmPasswd',
+    params: [{ login: loginParam }]
+  })
+    .then(() => {
+      success.value = true
+    })
+    .catch((err) => {
+      error.value = true
+      errorMessage.value = err.response?.data?.error?.message || 'Запрос на сброс пароля не создавался или уже активирован.'
+    })
+})
 </script>
