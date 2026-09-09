cae5e26676d12308ca7754ca58200ad1c55a1216
Tolybay.Kuanov <tolybay.zh@gmail.com>
2026-09-09 09:54:03 +0500

    upd

--- app-fish/fish-calc/frontend/src/pages/calcstock/TerminalPage.vue
+++ app-fish/fish-calc/frontend/src/pages/calcstock/TerminalPage.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Возвращаем q-dialog наверх, чтобы Quasar мог управлять его методами show/hide -->
-  <q-dialog ref="dialogRef" @hide="onDialogHide">
+  <q-dialog ref="dialogRef" @hide="onDialogHide" persistent>
     <q-card class="bg-grey-10 text-white q-pa-none" style="width: 750px; max-width: 90vw;">
       <q-card-section class="row items-center bg-grey-9 q-py-sm">
         <div class="text-subtitle2 text-white font-mono flex items-center q-gutter-x-sm">
